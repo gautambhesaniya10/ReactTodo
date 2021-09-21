17a527023f3a573a54e4e0b4e7f859b01b5a4c41
gautam <gb949308@gmail.com>
2021-09-21 18:12:43 +0530

    My First Todo App

---
+++ src/components/List.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import {FaEdit,FaTrash} from "react-icons/fa";
+
+ const List = ({ items , removeItem , editItem}) => {
+    return (
+        <div className="container">
+            {items.map((item) => {
+                const {id , title} = item;
+                return(
+                    <ul className="list-group list-group-flush" key={id}>
+                        <li className="list-group-item d-flax justify-content-between align-items-center">
+                            {title}
+                            <div style={{float:"right"}}>
+                                {/* <input type="checkbox" onChange={() => checkItem(id)} /> */}
+                                <button type="button" className="edit-btn" onClick={() => editItem(id)}><FaEdit/></button>
+                                <button type="button" className="delete-btn" onClick={() => removeItem(id)}><FaTrash/></button>
+                            </div>
+                        </li>
+                    </ul>
+                )
+            })}
+        </div>
+    )
+}
+
+export default List;
